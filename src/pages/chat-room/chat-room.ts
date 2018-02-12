1895ebd329f08871faba95d4780cc0408b2a8203
paul6ix <okporp@gmail.com>
2018-02-12 01:30:41 +0100

    Added chat room with socket server on localhost

---
+++ src/pages/chat-room/chat-room.ts
@@ -0,0 +1,85 @@
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import {Socket} from 'ng-socket-io';
+import {Observable} from "rxjs/Observable";
+
+/**
+ * Generated class for the ChatRoomPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-chat-room',
+  templateUrl: 'chat-room.html',
+})
+export class ChatRoomPage {
+  nickname;
+  messages = [];
+  message = "";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private socket: Socket, private toastCrtl: ToastController) {
+    let localname = JSON.parse(localStorage.getItem('loginToken')).user_nicename;
+    this.nickname = localname.toUpperCase();
+    this.socket.connect();
+    this.socket.emit('set-nickname', this.nickname);
+    this.getMessages().subscribe(message => {
+      this.messages.push(message);
+    });
+    this.getUsers().subscribe(data => {
+      let user = data['user'];
+      if (data['event'] === 'left') {
+        this.showToast('User left: ' + user);
+      }
+      else {
+        this.showToast('User Joined: ' + user)
+      }
+    });
+  }
+
+  sendMessage() {
+    this.socket.emit('add-message', {text: this.message});
+    this.message = '';
+
+  }
+
+  getUsers() {
+    let observable = new Observable(observer => {
+      this.socket.on('users-changed', data => {
+        observer.next(data);
+
+      })
+    });
+    return observable
+  }
+
+  getMessages() {
+    let observable = new Observable(observer => {
+      this.socket.on('message', data => {
+        observer.next(data);
+
+      })
+    });
+    return observable
+  }
+
+  showToast(msg) {
+    let toast = this.toastCrtl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ChatRoomPage');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave ChatRoomPage');
+    this.socket.disconnect()
+  }
+
+}
