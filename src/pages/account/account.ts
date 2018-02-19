77c5878b5f774d1ac6184c68327b265ec2f369a1
paul6ix <okporp@gmail.com>
2018-02-19 13:21:53 +0100

    fixed all errors and work on the npm errors

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {NavController, NavParams} from 'ionic-angular';
 import {LoginPage} from "../login/login";
 
 /**
@@ -9,7 +9,7 @@ import {LoginPage} from "../login/login";
  * Ionic pages and navigation.
  */
 
-@IonicPage()
+
 @Component({
   selector: 'page-account',
   templateUrl: 'account.html',
